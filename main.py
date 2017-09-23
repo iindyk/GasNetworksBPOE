3ddf323ee3d1f4007b369c6c2fcaf50e4222a8f7
iindyk <ihor.indyk@gmail.com>
2017-09-22 20:56:24 -0400

    data and constants declaration added

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import numpy as np
+import datetime
 from scipy.optimize import minimize
 from functions import *
 
 
 bnds, x0 = get_bounds_and_initialx()
 print(len(x0))
-con1 = {'type': 'eq', 'fun': lambda x: eq_constr(x)**2}
-con2 = {'type': 'ineq', 'fun': ineq_constr}
-cons = [con1, con2]
-sol = minimize(lambda x: avg_cost(x)**2, x0, bounds=bnds, constraints=cons)
+con1 = {'type': 'ineq', 'fun': lambda x: -eq_constr(x)**2}
+con2 = {'type': 'ineq', 'fun': lambda x: ineq_constr(x)}
+con3 = {'type': 'ineq', 'fun': lambda x: -compr_eq(x)**2}
+
+
+def bnds_constr(x):
+    ret = []
+    for i in range(len(x)):
+        if bnds[i][0] is not None:
+            ret.append(x[i]-bnds[i][0])
+        if bnds[i][1] is not None:
+            ret.append(bnds[i][1]-x[i])
+    return np.array(ret)
+con4 = {'type': 'ineq', 'fun': bnds_constr}
+cons = [con1, con2, con3, con4]
+options = {'maxiter': 10000}
+print(datetime.datetime.now())
+sol = minimize(avg_cost, x0, constraints=cons, method='COBYLA', options=options)
 print(sol.success)
 print(sol.message)
-print(sol.nit)
+#print(sol.nit)
